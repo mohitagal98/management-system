6671e818bd75b312a7f4c045730e73eacdc90c38
root <root@localhost.localdomain>
2020-05-14 13:11:53 +0530

    changed

--- abcd.php
+++ abcd.php
@@ -2,21 +2,7 @@
 <head>
 <title> hjgkjgkj </title>
 </head>
-<body><?php 
-include "include/db_config.php";
-
-$query = "SELECT * FROM admins";
-if(!$conn){
-echo mysqli_connect_error($conn);
-}
-else {
-echo "successful";
-}
-$row = mysqli_fetch_assoc(mysqli_query($conn,$query));
-
-echo $row['pass'];
-phpinfo();
-mysqli_close($conn);
-echo "<h5> vbnjhh </h5>"
+<body bgcolor='red'><?php 
+echo "Successful"
 ?></body>
 </html>
